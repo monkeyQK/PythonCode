0bc58169b3d7732ef8c169f1e53b19c051a71b9d
monkeyQK <155392114@qq.com>
2018-05-10 14:43:15 +0800

    add tools

---
+++ tools/list2xls.py
@@ -0,0 +1,23 @@
+from xlwt import Workbook
+
+
+def list2xls(li):
+    workbook = Workbook()
+    sheet = workbook.add_sheet('sheet1')
+    row = 0
+    for lines in li:
+        cols = 0
+        for i in lines:
+            sheet.write(row, cols, i)
+            cols += 1
+        row += 1
+    workbook.save(r'test.xls')
+
+
+def main():
+    li = [["id", "name", "aas"], ["001", "张三"]]
+    list2xls(li)
+
+
+if __name__ == '__main__':
+    main()
